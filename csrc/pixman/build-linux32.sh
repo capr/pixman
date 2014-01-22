7e4cbb66ba5853d156807f0e83caf16aadce9c00
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:25 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/pixman/build-linux32.sh
+++ csrc/pixman/build-linux32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS \
+gcc -O2 -s -static-libgcc \
 	-shared -o ../../bin/linux32/libpixman.so -Wall -I. \
 	-mmmx -msse2 -mfpmath=sse -DUSE_X86_MMX -DUSE_SSE2 \
 	-DPACKAGE=pixman \
--- csrc/pixman/build-mingw32.sh
+++ csrc/pixman/build-mingw32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS \
+gcc -O2 -s -static-libgcc \
 	-shared -o ../../bin/mingw32/pixman.dll -Wall -I. \
 	-mmmx -msse2 -mfpmath=sse -DUSE_X86_MMX -DUSE_SSE2 \
 	-DPACKAGE=pixman \
