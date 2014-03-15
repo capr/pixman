5fb85c31cc9f79d1229484f8018a2e76a393c8da
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-16 00:12:37 +0200

    osx64 build

---
+++ csrc/pixman/build-osx64.sh
@@ -0,0 +1,2 @@
+gcc -O2 `./files.sh` -shared -o ../../bin/osx64/libpixman.dylib -Wall -I. \
+	-mmmx -msse2 -mssse3 -mfpmath=sse -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread"
