b8894b8c6aeeca18d8abc042f62ee7f1da7c2f48
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 21:01:28 +0200

    rebuilt on osx without tls

--- csrc/pixman/build-osx32.sh
+++ csrc/pixman/build-osx32.sh
@@ -1,2 +1,3 @@
 gcc -arch i386 -O2 `./files.sh` -shared -install_name @loader_path/libpixman.dylib -o ../../bin/osx32/libpixman.dylib -Wall -I. \
-	-mmmx -msse2 -mssse3 -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread"
+	-mmmx -msse2 -mssse3 -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread" \
+	-mmacosx-version-min=10.6 -DPIXMAN_NO_TLS
