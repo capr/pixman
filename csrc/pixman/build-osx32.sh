8c6540cbc7bb5002209bea53ca9be84f87477126
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:09:12 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/pixman/build-osx32.sh
+++ csrc/pixman/build-osx32.sh
@@ -1,2 +1,2 @@
-gcc -arch i386 -O2 `./files.sh` -shared -o ../../bin/osx32/libpixman.dylib -Wall -I. \
+gcc -arch i386 -O2 `./files.sh` -shared -install_name @loader_path/libpixman.dylib -o ../../bin/osx32/libpixman.dylib -Wall -I. \
 	-mmmx -msse2 -mssse3 -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread"
--- csrc/pixman/build-osx64.sh
+++ csrc/pixman/build-osx64.sh
@@ -1,2 +1,2 @@
-gcc -arch x86_64 -O2 `./files.sh` -shared -o ../../bin/osx64/libpixman.dylib -Wall -I. \
-	-mmmx -msse2 -mssse3 -mfpmath=sse -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread"
+gcc -arch x86_64 -O2 `./files.sh` -shared -install_name @loader_path/libpixman.dylib -o ../../bin/osx64/libpixman.dylib -Wall -I. \
+	-mmmx -msse2 -mssse3 -DUSE_X86_MMX -DUSE_SSE2 -DUSE_SSSE3 -DUSE_GCC_INLINE_ASM -DHAVE_PTHREADS -DPACKAGE=pixman -DTLS="__thread"
