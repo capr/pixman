081a4dc9e94a11f0ccd2f02e8fbbe3f0c66d51ec
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:59:51 +0200

    unimportant

--- csrc/pixman/build-linux64.sh
+++ csrc/pixman/build-linux64.sh
@@ -1,2 +1,2 @@
-P=linux64 C="-fPIC -DHAVE_PTHREADS -DTLS=__thread" L="-s -static-libgcc" \
+P=linux64 C="-fPIC -include _memcpy.h -DHAVE_PTHREADS -DTLS=__thread" L="-s -static-libgcc" \
 	D=libpixman.so A=libpixman.a ./build.sh
